ece508e28279f89d13bd0a5155b12c311ecb5544
VladarTame <braskic.martin@gmail.com>
2025-10-29 20:20:10 +0100

    neda mi se otvarat merlin samo radi ovoga

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
+@REM call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\VsDevCmd.bat"
 echo Building...
 cl.exe %*
